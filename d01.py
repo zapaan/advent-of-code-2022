b693872d008ebaa112f99f2ae002fb9e5ac81c1a
Francky <francky.blondy@papernest.com>
2022-12-01 14:12:30 +0100

    remove cursor bullshit

--- d01.py
+++ d01.py
@@ -1,15 +1,12 @@
 from lib import readlines
 
 
-elves = []
-cur_elf = 0
-elves.append(0)
+elves = [0]
 for l in readlines():
     if not l:
         elves.append(0)
-        cur_elf += 1
     else:
-        elves[cur_elf] += int(l)
+        elves[-1] += int(l)
 
 elves.sort()
 print(elves[-1])
